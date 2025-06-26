19a9c5d2163facf093740ee2e8171fc11d78d64a
Rianat Abbas <rianat_abbas1@baylor.edu>
2025-06-26 17:32:57 -0500

    refactor: Create update_bullets() method to clean up run_game()

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
     def __init__(self):
+        """Initialize the game and create resources."""
         pygame.init()
         self.settings = Settings()
 
@@ -19,19 +20,15 @@ class AlienInvasion:
         self.bullets = pygame.sprite.Group()
 
     def run_game(self):
+        """Main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Remove bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
+        """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -41,6 +38,7 @@ class AlienInvasion:
                 self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
+        """Respond to key presses."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
@@ -51,18 +49,29 @@ class AlienInvasion:
             sys.exit()
 
     def _check_keyup_events(self, event):
+        """Respond to key releases."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
-        """Create a new bullet if limit not reached."""
+        """Create a new bullet and add it to the bullets group if limit not reached."""
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old ones."""
+        self.bullets.update()
+
+        # Remove bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
+        """Update images on the screen and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
